6492c59dd3220b0a70215c36c6d31d3e44451567
Enorian <enorian.rajo@gmail.com>
2026-05-24 22:41:29 +0200

    Add missing entry in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from "./core/check-utils";
 export * from "./core/database-modules";
-export * from "./core/database-modules";
-export * from "./core/decorators";
+export * from "./core/interfaces/error.interface"
 
 // User Microservice
 export * from "./core/services/users";
