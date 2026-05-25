e3c581d437857f33f5b5d791b00a348251caae27
Enorian <enorian.rajo@gmail.com>
2026-05-25 14:59:14 +0200

    Move some methods from common to gateway

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
-export * from "./core/check-utils";
 export * from "./core/database-modules";
-export * from "./core/interfaces/error.interface"
 
 // User Microservice
 export * from "./core/services/users";
