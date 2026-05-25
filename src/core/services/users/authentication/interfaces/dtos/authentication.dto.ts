d8d81728bbf33ba8b93f262c150d25a301c5c94d
Red-Hide <guigui67480@gmail.com>
2026-05-25 14:11:30 +0200

    Add Swagger decorators to LoginDto and LoginResponseDto

--- src/core/services/users/authentication/interfaces/dtos/authentication.dto.ts
+++ src/core/services/users/authentication/interfaces/dtos/authentication.dto.ts
@@ -1,8 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginDto {
+  @ApiProperty({ description: 'The email address of the account' })
   email: string;
+  @ApiProperty({ description: 'The password of the account' })
   password: string;
 }
 
 export class LoginResponseDto {
+  @ApiProperty({ description: 'Access token given by the backend' })
   access_token: string;
 }
