27e92589a21566927f9d58e711f28b7b89ace58b
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2026-06-08 16:27:16 +0200

    Add typeorm naming strategy in database module

--- src/core/database-modules.ts
+++ src/core/database-modules.ts
@@ -2,6 +2,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DynamicModule } from '@nestjs/common';
 import { DataSource, DataSourceOptions } from 'typeorm';
+import SnakeNamingStrategy from 'typeorm-naming-strategy';
 
 type DbType = DataSourceOptions['type'];
 
@@ -15,16 +16,18 @@ export const createDatabaseModule = (): DynamicModule =>
   TypeOrmModule.forRootAsync({
     imports: [ConfigModule],
     inject: [ConfigService],
-    useFactory: (configService: ConfigService) => ({
-      type: configService.get<DbType>('DB_TYPE', 'postgres'),
-      host: configService.get<string>('DB_HOST', 'localhost'),
-      port: configService.get<number>('DB_PORT', 5432),
-      username: configService.get<string>('DB_USER', 'postgres'),
-      password: configService.get<string>('DB_PASSWORD', 'postgres'),
-      database: configService.get<string>('DB_NAME'),
-      autoLoadEntities: true,
-      synchronize: false,
-    } as DataSourceOptions),
+    useFactory: (configService: ConfigService) =>
+      ({
+        type: configService.get<DbType>('DB_TYPE', 'postgres'),
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: configService.get<number>('DB_PORT', 5432),
+        username: configService.get<string>('DB_USER', 'postgres'),
+        password: configService.get<string>('DB_PASSWORD', 'postgres'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false,
+        namingStrategy: new SnakeNamingStrategy(),
+      }) as DataSourceOptions,
   });
 
 /**
@@ -43,4 +46,5 @@ export const createDatabase = (): DataSource =>
     database: process.env.DB_NAME || 'entropy_users_db',
     entities: ['src/**/*.entity{.ts,.js}'],
     migrations: ['src/database/migrations/*{.ts,.js}'],
+    namingStrategy: new SnakeNamingStrategy(),
   } as DataSourceOptions);
