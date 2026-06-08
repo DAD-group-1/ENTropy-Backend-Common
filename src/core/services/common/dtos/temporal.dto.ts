27801dee8d3a930054c825b64075110c157fcc98
En0ri4n <50873744+En0ri4n@users.noreply.github.com>
2026-06-08 17:21:39 +0200

    Add temporal query dto

---
+++ src/core/services/common/dtos/temporal.dto.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { PaginatedResponseDto } from './pagination.dto';
+
+/**
+ * DTO for temporal queries, containing a start date and an end date.
+ */
+export class TemporalQueryDto {
+  @ApiProperty({
+    description: 'The start date of the temporal query',
+    example: '2024-01-01T00:00:00.000Z',
+  })
+  startDate: Date;
+
+  @ApiProperty({
+    description: 'The end date of the temporal query',
+    example: '2024-12-31T23:59:59.999Z',
+  })
+  endDate: Date;
+}
+
+export class TemporalPaginatedQueryDto extends PaginatedResponseDto(
+  TemporalQueryDto,
+) {}
+
+export class TemporalSearchQueryDto extends TemporalQueryDto {
+  @ApiProperty({
+    description: 'The ID to search for within the temporal range',
+    example: 123,
+  })
+  id: number;
+}
