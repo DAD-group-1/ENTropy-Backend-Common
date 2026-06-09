7b1690e6493b2e67030ed848d14b800145ceff45
Anthony <schneppanthony@gmail.com>
2026-06-09 08:43:49 +0200

    Added payment method to the dto

--- src/core/services/billing/payments/interfaces/dtos/payment.dto.ts
+++ src/core/services/billing/payments/interfaces/dtos/payment.dto.ts
@@ -87,6 +87,9 @@ export class PaymentResponseDto {
 
   @ApiProperty()
   user: UserResponseDto;
+
+  @ApiProperty()
+  payment_method: UserResponseDto;
 }
 
 export class PaymentListResponseDto extends PaginatedResponseDto(
